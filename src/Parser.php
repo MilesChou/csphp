1711e1a896af48a85be66a72ffead09198d42190
MilesChou <jangconan@gmail.com>
2019-05-24 23:05:53 +0800

    Add basic parser function

--- src/Parser.php
+++ src/Parser.php
@@ -4,4 +4,22 @@ namespace MilesChou\Csphp;
 
 class Parser
 {
+    /**
+     * @param string $csp
+     * @return array
+     */
+    public function parse($csp)
+    {
+        $policies = array_map(function ($policy) {
+            return explode(' ', trim($policy));
+        }, explode(';', $csp));
+
+        return array_reduce($policies, function ($carry, $policy) {
+            $key = array_shift($policy);
+
+            $carry[$key] = $policy;
+
+            return $carry;
+        }, []);
+    }
 }
--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -28,8 +28,13 @@ class ParserTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function shouldBeOkay()
+    public function shouldBeParseForGitHubCsp()
     {
-        $this->assertInstanceOf(Parser::class, $this->target);
+        $csp = "default-src 'none'; base-uri 'self'; block-all-mixed-content; connect-src 'self' uploads.github.com www.githubstatus.com collector.githubapp.com api.github.com www.google-analytics.com github-cloud.s3.amazonaws.com github-production-repository-file-5c1aeb.s3.amazonaws.com github-production-upload-manifest-file-7fdce7.s3.amazonaws.com github-production-user-asset-6210df.s3.amazonaws.com wss://live.github.com; font-src github.githubassets.com; form-action 'self' github.com gist.github.com; frame-ancestors 'none'; frame-src render.githubusercontent.com; img-src 'self' data: github.githubassets.com identicons.github.com collector.githubapp.com github-cloud.s3.amazonaws.com *.githubusercontent.com; manifest-src 'self'; media-src 'none'; script-src github.githubassets.com; style-src 'unsafe-inline' github.githubassets.com";
+
+        $actual = $this->target->parse($csp);
+
+        $this->assertArraySubset(['default-src' => ["'none'"]], $actual);
+        $this->assertArraySubset(['base-uri' => ["'self'"]], $actual);
     }
 }
